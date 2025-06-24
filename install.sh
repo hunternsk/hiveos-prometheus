c0bacef10cf30a2d581a0557348fe457da883f5a
Serge Klepikov <hunternsk@gmail.com>
2025-06-24 21:28:26 +0700

    installer fix url

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 mv /usr/local/bin/hiveos-prometheus /usr/local/bin/hiveos-prometheus.bak
-wget "$1"/hiveos_prometheus/hiveos-prometheus -qO /usr/local/bin/hiveos-prometheus
+wget "$1"/hiveos-prometheus/hiveos-prometheus -qO /usr/local/bin/hiveos-prometheus
 chmod +x /usr/local/bin/hiveos-prometheus
-wget "$1"/hiveos_prometheus/hiveos-prometheus.service -qO /etc/systemd/system/hiveos-prometheus.service
+wget "$1"/hiveos-prometheus/hiveos-prometheus.service -qO /etc/systemd/system/hiveos-prometheus.service
 systemctl daemon-reload
 systemctl enable --now hiveos-prometheus.service

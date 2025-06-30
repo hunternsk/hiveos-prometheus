8c67516d5b9ef175a8c96824c1a48ed3fa4143bf
Serge <hunternsk@users.noreply.github.com>
2025-06-30 13:52:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 mv /usr/local/bin/hiveos-prometheus /usr/local/bin/hiveos-prometheus.bak
 wget "$1"/hiveos-prometheus/hiveos-prometheus -qO /usr/local/bin/hiveos-prometheus
 chmod +x /usr/local/bin/hiveos-prometheus
-wget "$1"/hiveos-prometheus/hiveos-prometheus.service -qO /etc/systemd/system/hiveos-prometheus.service
+wget https://raw.githubusercontent.com/hunternsk/hiveos-prometheus/refs/heads/main/hiveos-prometheus.service -qO /etc/systemd/system/hiveos-prometheus.service
 systemctl daemon-reload
 systemctl enable --now hiveos-prometheus.service
